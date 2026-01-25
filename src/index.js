36aff7dbe2442cc594bf1ebbf2892834d86ef188
lukashe14 <lukas.heino@metropolia.fi>
2026-01-25 22:12:05 +0200

    ..

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import {deleteItemById, getItemById, getItems, postNewItem, putItemById} from './items.js';
+import {deleteUserById, getUsers,getUserById, putUserById, postUser, postLogin } from './users.js';
 const hostname = '127.0.0.1';
 const app = express();
 const port = 3000;
@@ -28,7 +29,27 @@ app.delete('/api/items/:id', deleteItemById);
 app.post('/api/items', postNewItem);
 
 //User resource endpoints
-app.get('')
+app.get('/api/users', getUsers);
+
+
+// Post new user
+app.post('api/users', postUser);
+
+
+//post user login
+app.post('/api/users/login', postLogin);
+
+
+//itemssistä löytyy malli
+//TODO: get user by id
+app.get('api/users/:id', getUserById);
+//app.get('/api/users/:id');
+
+//TODO: put user by id
+app.put('/api/users/:id', putUserById);
+
+//TODO: delete user by id
+app.delete('/api/users/:id', deleteUserById);
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
---
+++ src/users.js
@@ -0,0 +1,102 @@
+//TODO: add users mock data and endpoints
+const users = [
+  {
+    id: 1,
+    username: "johndoe",
+    password: "password1",
+    email: "johndoe@example.com"
+  },
+  {
+    id: 2,
+    username: "janedoe",
+    password: "password2",
+    email: "janedoe@example.com"
+  },
+  {
+    id: 3,
+    username: "bobsmith",
+    password: "password3",
+    email: "bobsmith@example.com"
+  }
+];
+
+const getUsers = (request, response)=> {
+  // ÄLÄ IKINÄ lähetä salasanoja http vastauksessa
+  for (let i=0; i<users.length; i++) {
+    delete users[i].password;
+    //sensuroi käyttäjän emailin
+    //users[i].email = 'sensores';
+  }
+  response.json(users);
+};
+
+//TODO: getUserByID
+
+// käyttäjän lisäys (rekisteröinti)
+const postUser = (pyynto, vastaus) => {
+  const newUser = pyynto.body;
+  // Uusilla käyttäjillä pitää olla kaikki vaaditut ominaisuudet tai palautetaan virhe
+  // itse koodattu erittäin yksinkertainen syötteen validointi
+  if (!(newUser.username && newUser.password && newUser.email)) {
+    return vastaus.status(400).json({error: 'required fields missing'});
+  }
+
+ // HUOM: ÄLÄ ikinä loggaa käyttäjätietoja ensimmäisten pakollisten testien jälkeen!!! (tietosuoja)
+  //console.log('registering new user', newUser);
+  const newId = users[users.length - 1].id + 1;
+  // luodaan uusi objekti, joka sisältää id-ominaisuuden ja kaikki newUserObjektin
+  // ominaisuudet ja lisätään users-taulukon loppuun
+  users.push({id: newId, ...newUser});
+  delete newUser.password;
+  console.log('users', users);
+  vastaus.status(201).json({message: 'new user added', user_id: newId});
+};
+
+const postLogin = (req, res) => {
+  const {username, password} = req.body;
+  // haetaan käyttäjä-objekti käyttäjän nimen perusteella
+  const userFound = users.find(user => username === user.username);
+  if (userFound) {
+    if (userFound.password === password) {
+      delete userFound.password;
+      return res.json({message: 'login ok', user: userFound});
+    }
+    return res.status(403).json({error: 'invalid password'});
+  }
+  res.status(404).json({error: 'user not found'});
+};
+
+
+const getUserById = (req, res) => {
+  console.log('getting user id:', req.params.id);
+  const userFound = users.find((user) => user.id == req.params.id);
+  if (userFound) {
+    res.json(userFound);
+  } else {
+    res.status(404).json({message: 'user not found'});
+  }
+};
+
+const putUserById = (req, res) => {
+  console.log('updating user id:', req.params.id);
+  const userIndex = users.findIndex((item) => item.id == req.params.id);
+  if (userIndex !== -1) {
+    users[userIndex] = {...users[userIndex], ...req.body};
+    res.json({message: 'user updated', user: users[userIndex]});
+  } else {
+    res.status(404).json({message: 'user not found'});
+  }
+};
+
+const deleteUserById = (req, res) => {
+  console.log('deleting user id:', req.params.id);
+  const userIndex = users.findIndex((user) => user.id == req.params.id);
+  if (userIndex !== -1) {
+    users.splice(userIndex, 1);
+    res.json({message: 'user deleted'});
+  } else {
+    res.status(404).json({message: 'user not found'});
+  }
+};
+
+export {getUsers, postUser, postLogin, getUserById, putUserById, deleteUserById};
